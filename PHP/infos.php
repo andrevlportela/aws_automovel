318eee351e85f2e9988540efd2bfee43550815d3
André Portela <131263478+andrevlportela@users.noreply.github.com>
2023-08-25 14:10:11 -0300

    aws

---
+++ PHP/infos.php
@@ -0,0 +1,7 @@
+<?php
+// Configuração do banco de dados
+$host = "db-andre.c4aq5ihzelsg.us-east-1.rds.amazonaws.com";
+$username = "andre";
+$password = "@123qweasd";
+$database = "db_concessionaria";
+?>
