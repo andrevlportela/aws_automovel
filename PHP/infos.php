f151a8f7a1d3c4e307c1a721a546e0008d5a78a5
André Portela <131263478+andrevlportela@users.noreply.github.com>
2023-08-25 15:58:30 -0300

    Update infos.php

--- PHP/infos.php
+++ PHP/infos.php
@@ -2,6 +2,6 @@
 // Configuração do banco de dados
 $host = "db-andre.c4aq5ihzelsg.us-east-1.rds.amazonaws.com";
 $username = "andre";
-$password = "@123qweasd";
+$password = "Att3y7HVn8P9qYA9DToZ";
 $database = "db_concessionaria";
 ?>
